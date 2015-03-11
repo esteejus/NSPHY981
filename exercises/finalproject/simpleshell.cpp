5423c51dfb5634f6b767022727a20dc5586f3812
justin <justin@localhost.localdomain>
2015-03-11 13:44:35 -0400

    final work. matrix works. cannot get armadillo to calculate the eigenvalues

--- exercises/finalproject/simpleshell.cpp
+++ exercises/finalproject/simpleshell.cpp
@@ -15,21 +15,27 @@ void combinationUtil(int*, int*, int, int,int, int, int, rowvec &sd,int &counter
 
 int main(){
      
- int npart=4;
- int splevels=6;
- int nsd=nChoosek(splevels,npart);
+  int npart=4; //number of particles  
+  int splevels=8;//number of sp levels in pairing model 2*#ofdegenerate levels
+  int nsd=nChoosek(splevels,npart);//number of SD
  mat sd(nsd,npart);//all possible slater det.
- mscheme(npart,nsd,splevels,sd);//sorts only pairs
+ mscheme(npart,nsd,splevels,sd);//sorts only pairs use this to get the actual # of SD in the pairing model store as nsd again
  mat hamilt(nsd,nsd);
- hamilt.zeros();
+ hamilt.zeros();//zero out hamiltonian
  for(int i=0;i<nsd;i++){
    rowvec wf = sd.row(i);//wave function(SD) for bra
    onebody(wf,sd,nsd,hamilt,i);
    twobody(wf,sd,nsd,splevels,hamilt,i);
-   
  }  
-  hamilt.print();
-  
+
+ cout<<endl<<endl;
+   hamilt.print();
+
+   //cannot for the life of me get aramadillo to calculate the eigen values.
+   //uncomment to see what im talking about 
+   // vec eig = eig_sym(hamilt);
+ // eig.print();
+
 
   return 0 ;
   
@@ -50,8 +56,6 @@ void mscheme(int npart,int &nsd,int splevels, mat &tempsd){
    z=0;//set counter back to 0 
  tempsd.zeros(); 
  combinationUtil(arr,data,0,splevels-1,0,npart,nsd,sd,z,tempsd); 
- //sd.print(); 
- // tempsd.print();
  
 
 } 
@@ -62,6 +66,9 @@ void mscheme(int npart,int &nsd,int splevels, mat &tempsd){
    start & end ---> Staring and Ending indexes in arr[]
    index  ---> Current index in data[]
    r ---> Size of a combination to be printed */
+
+// this code caluclates the combinations of all SD for a given splevel scheme and particle number
+//this code was modified from some code online to the purposes i needed 
 void combinationUtil(int arr[], int data[], int start, int end, int index, int r, int nsd, rowvec &sd,int &counter,mat &tempwf)
    {
      // Current combination is ready to be printed, print it
@@ -69,32 +76,28 @@ void combinationUtil(int arr[], int data[], int start, int end, int index, int r
 	 if (index == r)
 	   {
 	     
-	     
+
+	     //this part only grabs states that have pairs  	     
 	     for (int j=0; j<r; j++){
 	       
-	       //if((j+1)%2==0 && data[j]==data[j-1]+1 && data[j]%2==0){
+	       if((j+1)%2==0 && data[j]==data[j-1]+1 && data[j]%2==0){
 		 sd(j)=data[j];
-		 //sd(j-1)=data[j-1];
-		 //}	 // cout<<"counter"<<counter<<endl;
-		 // }
-	       //else if((j+1)%2 && (data[j]!=data[j-1]+1)){
-		  //if(data[j]==2 && data[j-1]==1)cout<<j<<endl;
-		  //sd.zeros();
-		 }
-	       int product=1;
+		 sd(j-1)=data[j-1];}
+	       else sd(j)=0;//here i assign the elem zero if it is not in a pair state important for below 
+	     }
+	     //since any non pair states have at least one 0 element
+	     //the product of all the elems should be zero for non pair SD and non zero for SD
+	     //i find these good SD here
+	     int product=1;
 	       for(int l=0;l<r;l++){
 		 product=sd(l)*product;
 	       }
-	       //	 if(product!=0){
-	       //  counter++;
-	       //	   tempwf.insert_rows(counter-1,sd);
-		   //	   sd.print();
-		   //}
-	       
-	   
-	     
-	     //printf("\n");
-	     sd.print();
+	       if(product!=0){
+	         counter++;
+		 tempwf.insert_rows(counter-1,sd);//store the good SD in the tempwf I pass by ref
+
+	       }
+	      
 	     return;
 	   }
    }
@@ -110,9 +113,10 @@ void combinationUtil(int arr[], int data[], int start, int end, int index, int r
        }
    }
 
+
+//calculate the one body matrix elements
 void onebody(rowvec wf,mat sd, int nsd, mat &hamilt, int &i){
-  //int matxel=0; 
- 
+
   for(int j=0;j<nsd;j++){
     rowvec wf2 = sd.row(j);//bra vector
     if(equal(wf,wf2)==true){//bra and ket test
@@ -123,7 +127,7 @@ void onebody(rowvec wf,mat sd, int nsd, mat &hamilt, int &i){
 	  //take 2*(p-1) where as_scalar(wf(k+1)) is p
 	  //the 2* is for both particles since 
 	  //i am only counting the even labels
-	  //	  hamilt(i,j)=hamilt(i,j)+2*(as_scalar(wf(k+1)/2-1));
+	   hamilt(i,j)=hamilt(i,j)+2*(as_scalar(wf(k+1)/2-1));
  
 	}
       }
@@ -133,9 +137,9 @@ void onebody(rowvec wf,mat sd, int nsd, mat &hamilt, int &i){
   return ;
   }
 
-
+//calculate the two body matrix elements
 void twobody(rowvec wf,mat sd, int nsd,int splevels, mat &hamilt, int &i){ 
-  int me=-2;//matrix element
+  int me=0;//set here matrix element
   
 rowvec temp(wf.n_elem);//temp wf for calculating matrix elements
     
@@ -147,9 +151,8 @@ rowvec temp(wf.n_elem);//temp wf for calculating matrix elements
 	for(int p=1;p<=splevels;p++){//loop over pair creation
 	  for(int q=1;q<=splevels;q++){//loop over pair anilhiation
 	    if(r%2==0 && !(p%2==0) && !(q%2==0)){//need to look at pairs 
-	      //	      cout<<"Before"<<" "<<p<<p+1<<q<<q+1<<endl;
 	      temp=wf;
-	      //temp.print();
+	     
 	      //checking to see if pair state exists to be anihilated 
 	      //i.e. if temp=|1234>
 	      //|1234>-|1200>=|0034>
@@ -161,30 +164,34 @@ rowvec temp(wf.n_elem);//temp wf for calculating matrix elements
 		temp(r)=p;
 		temp(r+1)=p+1;
 		if(equal(temp,wf2)==true)hamilt(i,j)+=me;
-
-		//		cout<<"After"<<endl;
-		//temp.print();
-		//		hamilt.print();
-		//cout<<endl<<endl;
-	      }
+	      }		
 	    }
 	  }	  
 	}
       }    
     }
-  return;
+    return;
 }
 
+
+//function to see if two vectors are equal
   bool equal(rowvec wf, rowvec wf2){
     bool flag=true;
     if(wf.n_elem!=wf2.n_elem)cout<<"Num elements not equal"<<endl; 
-    for(int i=0;i<wf.n_elem;i++){
-      if(wf(i)!=wf2(i)) flag=false;
-    }
+    // need to stort the elem in the vector so that we can compare element by elem
+    //to see if they are equal
+    rowvec a = sort(wf);
+    rowvec b = sort(wf2);
+     for(int i=0;i<wf.n_elem;i++){
+       if(a(i)!= b(i)) flag=false;
+	   }
+	 
     return flag;
   }
 
+
 //barrowed from online
+//combination calculation
 unsigned nChoosek( unsigned n, unsigned k )
 {
   if (k > n) return 0;
